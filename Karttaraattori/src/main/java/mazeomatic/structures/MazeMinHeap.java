be41b307bd01a89f9f78618855b413a36aca83ce
majormalfunk <32271489+majormalfunk@users.noreply.github.com>
2018-08-27 20:57:16 +0300

    Korjattu rikki mennyt (vielä) tyhjä minimikeko-luokka

--- Karttaraattori/src/main/java/mazeomatic/structures/MazeMinHeap.java
+++ Karttaraattori/src/main/java/mazeomatic/structures/MazeMinHeap.java
@@ -1,3 +1,18 @@
-Unstaged changes after reset:
-M	Karttaraattori/src/main/java/mazeomatic/structures/MazeMinHeap.java
-D	Karttaraattori/src/main/java/mazeomatic/structures/MazePriorityQueue.java
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mazeomatic.structures;
+
+/**
+ *
+ * @author jaakkovilenius
+ */
+public class MazeMinHeap {
+    
+    public MazeMinHeap() {
+        
+    }
+    
+}
