e5f03d427fd88d952a1b128c592fa26f6509172a
majormalfunk <32271489+majormalfunk@users.noreply.github.com>
2018-09-07 23:08:02 +0300

    Oma random

--- Karttaraattori/src/main/java/mazeomatic/structures/MazeRandomCongruential.java
+++ Karttaraattori/src/main/java/mazeomatic/structures/MazeRandomCongruential.java
@@ -5,23 +5,33 @@
  */
 package mazeomatic.structures;
 
-import java.util.Random; // WE NEED TO REPLACE THIS WITH AN IMPLEMENTATION OF OUR OWN
-
 /**
- *
+ * Class to cfeate pseudorandom numbers for placing rooms in the map.
+ * This follows the Microsoft implementation at: https://rosettacode.org/wiki/Linear_congruential_generator
+ * 
  * @author jaakkovilenius
  */
 public class MazeRandomCongruential implements MazeRandom {
+        
+    long seed;
+    final static long MULTIPLIER = 214013;
+    final static long ADDER = 2531011;
+    final static long DIVIDER = 65536;
     
-    private Random random;
     
     public MazeRandomCongruential() {
-        random = new Random();
+        
+        seed = System.currentTimeMillis();
+        seed = seed % (long)Integer.MAX_VALUE;
+        
     }
 
     @Override
     public int nextInt(int n) {
-        return random.nextInt(n);
+        
+        seed = (seed * MULTIPLIER + ADDER) % (long)Integer.MAX_VALUE;
+        return (int) (seed / DIVIDER) % n;
+        
     }
     
 }
